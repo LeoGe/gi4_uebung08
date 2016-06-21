507acbb860bfa79093ce8c360be04c98e3ac9330
LeoGe <leonie@geyernest.de>
2016-06-21 20:53:20 +0200

    some files weren't uploaded

--- cntdwn.asm
+++ cntdwn.asm
@@ -1,8 +1,6 @@
 ;Zahlen sind im Hexadezimalsystem angegeben
 ;dahinter Adressmodus: es wird nicht ein Register adressiert. Register: ein Register wird adressiert.
 
-
-
 0000  SECTION .text	;Pseudo
 0000	extern printf	;Pseudo
 0000	extern exit	;Pseudo
@@ -21,29 +19,33 @@
 001E	cmp ebx , dword [ebp-4]	;(7) opCode+Register+Adressmodus+Displacement
 0025	je ende	;(6) opCode+Adressmodus+Immediate
 	
-002B	mov byte [msg+msg_len -4], bl	;(B) opCode+Adressmodus+Register+Displacement+Immediate
+002B	mov byte [msg+msg_len -4], bl	;(7) opCode+Adressmodus+Register+Immediate
 	
-0036	push dword msg	;(6) opCode+Adressmodus+Immediate
-003C	call printf	;(6) opCode+Adressmodus+Immediate
-0048	add esp, 4	;(7) opCode+Resgister+Adressmodus+Immediate
+0032	push dword msg	;(6) opCode+Adressmodus+Immediate
+0038	call printf	;(6) opCode+Adressmodus+Immediate
+003E	add esp, 4	;(7) opCode+Resgister+Adressmodus+Immediate
 	
-004F	dec ebx		;(2) OpCode+Register
-0051	jmp schleife	;(6) opCode+Adressmodus+Immediate
+0045	dec ebx		;(2) OpCode+Register
+0047	jmp schleife	;(6) opCode+Adressmodus+Immediate
 	
-0057  ende:	;Pseudo
-0057	add esp, 4	;(7) opCode+Register+Adressmodus+Immediate
-005B	pop ebp		;(2) opCode+Register
+004D  ende:	;Pseudo
+004D	add esp, 4	;(7) opCode+Register+Adressmodus+Immediate
+0054	pop ebp		;(2) opCode+Register
 	
-005D	push dword 0	;(6) opCode+Adressmodus+Immediate
-0063	call exit	;(6) opCode+Adressmodus+Immediate
-0069	add esp, 4	;(7) opCode+Register+Adressmodus+Immediate
+0056	push dword 0	;(6) opCode+Adressmodus+Immediate
+005C	call exit	;(6) opCode+Adressmodus+Immediate
+0062	add esp, 4	;(7) opCode+Register+Adressmodus+Immediate
 	
-0070	mov eax, 1	;(7) opCode+Register+Adressmodus+Immediate
-0077	mov ebx, 0	;(7) opCode+Register+Adressmodus+Immediate
-007E	int 0x80	;(6) opCode+Adressmodus+Immediate
+0069	mov eax, 1	;(7) opCode+Register+Adressmodus+Immediate
+0070	mov ebx, 0	;(7) opCode+Register+Adressmodus+Immediate
+0077	int 0x80	;(6) opCode+Adressmodus+Immediate
 	
-0000  SECTION .data	;Pseudo
-0000	CR equ 13	;Pseudo
-0000	LF equ 10	;Pseudo
-0000	msg db "Hello World. ebx = ?", CR, LF, 0 ;Pseudo (17)
-0017	msg_len equ $ - msg	;Pseudo
+007D  SECTION .data	;Pseudo
+007D	CR equ 13	;Pseudo
+007D	LF equ 10	;Pseudo
+007D	msg db "Hello World! ebx = ?", CR, LF, 0 ;Pseudo (17)
+0094	msg_len equ $ - msg	;Pseudo
+
+;Länge des Codesegments: 007D (125 in dezimal)
+;Länge des Datensegments: 0017 (23 in dezimal)
+;Gesamtlänge: 0094 (148 in dezimal)
